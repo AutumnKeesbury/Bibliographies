05f5b2391685d66262c6f54bc7cf94bd5fb1169f
AutumnKeesbury <angsukeesbury@gmail.com>
2025-11-06 20:53:53 -0500

    clean main.py, write keywords.py -> CWStudies_v3.bib

--- automation/main.py
+++ automation/main.py
@@ -26,9 +26,6 @@ for entry in library.entries:
     key = entry.key
     if keyMatch(key):
         key_split = re.split(r":", key)
-        # head = re.search(r"\d|\d{2}:", key)
-        # frame = re.search(r"\d{4}", key)
-        # index = entry.get("index")
 
         reel = key_split[1]
         key_reel.append(reel)
@@ -39,12 +36,7 @@ for entry in library.entries:
             entry.__setitem__("index", f"f{key_split[2]}")
 
         index = str(entry.__getitem__("index"))
-        # index_getter = r"`\d+`"
-        # index = re.search(index, index_getter)
 
-        # print(f"reel: {reel}")
-        # print(f"index: {index}")
-        # print(f"old key = {key}")
         indicies.append(entry.get("key"))
 
         key = f"ussc:{reel}:{index}"
--- keywords.py
+++ keywords.py
@@ -1,4 +1,20 @@
 import bibtexparser
-import re
 
 bibfile = "CivilWarStudies_reformatted.bib"
+library = bibtexparser.parse_file(bibfile)
+
+if len(library.failed_blocks) > 0:
+    print("Some blocks failed to parse. Check the entries of `library.failed_blocks`.")
+else:
+    print("All blocks parsed successfully.")
+
+for entry in library.entries:
+    if entry.entry_type == "misc":
+        entry.__setitem__("keywords", "primary")
+    else:
+        entry.__setitem__("keywords", "secondary")
+
+
+bibtexparser.write_file(
+    "CWStudies_v3.bib", library, bibtex_format=bibtexparser.BibtexFormat()
+)
